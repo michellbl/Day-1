4d9909c6231d08345086be97b92dee9fb1350c69
Michell Bezerra de Lima <michellblima@gmail.com>
2022-01-06 19:21:31 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#1. Create a greeting for your programa
+
+#2. Ask the user for the city that they grew up input
+
+#3.  Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show their band name.
+
+
+# My code is down here.
+
+print("Welcome stranger to the band name generator.")
+
+cityName = input("Which city did you grow up in?\n")
+
+petName = input("What is the name of your pet?\n")
+
+print("TADAAAAAH, your band name is: " + str(cityName) +" "+ str(petName))
